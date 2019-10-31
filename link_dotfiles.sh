94d8cd828e0f0b0f7030701b62d7a529d1c85d76
Quentin PLAZAR <quentinplaz@hotmail.com>
2019-10-31 14:38:12 -0700

    Basic xmonad config

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+function make_link {
+    target=$1 
+    src=$2 
+    if [ -e $src ]
+    then
+        echo "$src exists, skipping..."
+    else
+        mkdir -p `dirname $src` 
+        ln -s `pwd`/$target $src
+        echo "$src linked"
+    fi
+}
+
 # ln -s `pwd`/bashrc ~/.bashrc ##  NOT IMPLEMENTED
 # ln -s `pwd`/inputrc ~/.inputrc ##  NOT IMPLEMENTED
-ln -s `pwd`/vimrc ~/.vimrc ##  NOT IMPLEMENTED
-# ln -s `pwd`/xmonadrc ~/.config/.xmonad/xmonad.hs ##  NOT IMPLEMENTED
+
+make_link vimrc ~/.vimrc
+make_link xmonadrc ~/.xmonad/xmonad.hs
+
 # ln -s `pwd`/xsessionrc ~/.xsessionrc ##  NOT IMPLEMENTED
