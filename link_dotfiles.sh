bf7ca05537a2a0eda3cf3412a9f50e5616306bdf
Quentin PLAZAR <quentinplaz@hotmail.com>
2019-10-31 13:33:18 -0700

    Mark not implemented lines in linking script

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-ln -s `pwd`/vimrc ~/.vimrc
-ln -s `pwd`/xmonadrc ~/.config/.xmonad/xmonad.hs
-ln -s `pwd`/xsessionrc ~/.xsessionrc
+# ln -s `pwd`/bashrc ~/.bashrc ##  NOT IMPLEMENTED
+# ln -s `pwd`/inputrc ~/.inputrc ##  NOT IMPLEMENTED
+# ln -s `pwd`/vimrc ~/.vimrc ##  NOT IMPLEMENTED
+# ln -s `pwd`/xmonadrc ~/.config/.xmonad/xmonad.hs ##  NOT IMPLEMENTED
+# ln -s `pwd`/xsessionrc ~/.xsessionrc ##  NOT IMPLEMENTED
