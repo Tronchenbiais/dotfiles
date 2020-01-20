0b72f2580465e92f7b27e371ad1aa760b9b20562
Quentin PLAZAR <quentinplaz@hotmail.com>
2020-01-20 19:42:21 +0100

    Transparency on non-focused terminals only

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -23,6 +23,6 @@ make_link vifmrc ~/.config/vifm/vifmrc
 
 make_link xsessionrc ~/.xsessionrc
 make_link xmonad.hs ~/.xmonad/xmonad.hs
-make_link xmobar.hs ~/.xmobarrc
+make_link compton.conf ~/.config/compton.conf
 
 make_link img/middleEarth.jpg ~/wallpaper/middleEarth.jpg
