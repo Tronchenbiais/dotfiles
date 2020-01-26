222ec8e5b66ab37a7e9f149c369ef2fb892a9245
Quentin PLAZAR <quentinplaz@hotmail.com>
2020-01-26 18:30:15 +0100

    float and copy mpv for youtube videos

--- xmonad.hs
+++ xmonad.hs
@@ -1,10 +1,12 @@
-import Data.Map (fromList)
 import Data.List
+import Data.Map (fromList)
+import System.IO
 import XMonad
-import qualified XMonad.StackSet as W
+import XMonad.Actions.CopyWindow
 import XMonad.Actions.CycleWS
-import XMonad.Actions.PhysicalScreens
 import XMonad.Actions.OnScreen
+import XMonad.Actions.PhysicalScreens
+import XMonad.Actions.SpawnOn
 import XMonad.Config.Azerty
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
@@ -14,13 +16,15 @@ import XMonad.Layout.Spacing
 import XMonad.Layout.Tabbed
 import XMonad.Util.EZConfig
 import XMonad.Util.Run(spawnPipe) 
-import System.IO
+import qualified XMonad.StackSet as W
 
+-- General Settings
 myTerminal = "lxterminal"
 myBrowser  = "qutebrowser"
 mySreenLockCmd = "dm-tool lock"
-myVirtualWSs = ["1:dev", "2:web", "3:misc", "4:sys"]
+myVirtualWSs = ["1:web", "2:misc", "3:dev", "4:sys"]
 
+-- Independent (synced) screens
 nextVirtualWSname :: VirtualWorkspace -> VirtualWorkspace
 nextVirtualWSname vWorkSpace =
     case elemIndex vWorkSpace myVirtualWSs of
@@ -33,17 +37,25 @@ prevVirtualWSname vWorkSpace =
         Nothing -> head myVirtualWSs
         Just i -> myVirtualWSs !! (mod (i - 1) $ length myVirtualWSs)
 
-liftVirtualWSaction :: (VirtualWorkspace -> VirtualWorkspace) -> WindowSet -> WindowSet
-liftVirtualWSaction wsAction stackset =
+liftVirtualWSaction :: 
+    (PhysicalWorkspace -> WindowSet -> WindowSet) ->
+    (VirtualWorkspace -> VirtualWorkspace) -> WindowSet -> WindowSet
+liftVirtualWSaction action wsAction stackset =
     let (screen, vWS) = unmarshall $ W.currentTag stackset in
-        let action = W.greedyView $ marshall screen $ wsAction vWS in
-            onScreen action FocusCurrent screen stackset
+        let screenAction = action $ marshall screen $ wsAction vWS in
+            onScreen screenAction FocusCurrent screen stackset
 
 prevVirtualWS :: WindowSet -> WindowSet
-prevVirtualWS = liftVirtualWSaction prevVirtualWSname
+prevVirtualWS = liftVirtualWSaction W.greedyView prevVirtualWSname
+
+shiftPrevVirtualWS :: WindowSet -> WindowSet
+shiftPrevVirtualWS = liftVirtualWSaction W.shift prevVirtualWSname
 
 nextVirtualWS :: WindowSet -> WindowSet
-nextVirtualWS = liftVirtualWSaction nextVirtualWSname
+nextVirtualWS = liftVirtualWSaction W.greedyView nextVirtualWSname
+
+shiftNextVirtualWS :: WindowSet -> WindowSet
+shiftNextVirtualWS = liftVirtualWSaction W.shift nextVirtualWSname
 
 onAllScreens' :: (WindowSet -> WindowSet) -> WindowSet -> WindowSet
 onAllScreens' action windowSet =
@@ -62,6 +74,7 @@ onAllScreens action workspace windowSet =
 
 wpKeys = ["&","é","\"","'","(","-","è","_","ç","à"]
 
+-- Key bindings
 myKeys conf = 
     [ ("M-b", spawn myBrowser)
     , ("M-w", kill)
@@ -76,8 +89,16 @@ myKeys conf =
         ])
     , ("M-,", windows $ onAllScreens' prevVirtualWS)
     , ("M-;", windows $ onAllScreens' nextVirtualWS)
-    , ("M-S-,", windows $ prevVirtualWS)
-    , ("M-S-;", windows $ nextVirtualWS)
+    , ("M-S-,", composeAll 
+        [ windows $ shiftPrevVirtualWS
+        , windows $ onAllScreens' prevVirtualWS
+        ])
+    , ("M-S-;", composeAll 
+        [ windows $ shiftNextVirtualWS
+        , windows $ onAllScreens' nextVirtualWS
+        ])
+    , ("M-C-,", windows $ prevVirtualWS)
+    , ("M-C-;", windows $ nextVirtualWS)
     ] ++
     [(("M-S-" ++ key), windows $ onCurrentScreen W.shift workspace)
         | (workspace, key) <- zip (workspaces' conf) wpKeys ] 
@@ -89,6 +110,7 @@ myKeys conf =
         | (screen, key) <- zip [0..] ["a","z","e","r"]
         , (mod, action) <- [("M-", viewScreen def), ("M-S-", sendToScreen def)]]
 
+-- Spacing
 mySpacedLayout =
     spacingRaw
         -- Use smart spacing (space only when 2 or more windows are open)
@@ -98,13 +120,16 @@ mySpacedLayout =
         -- Window border
         (Border 5 5 5 5) True 
 
+-- Layouts
 myBaseLayout = Tall 1 (2/100) (1/2) |||
                Mirror (Tall 1 (2/100) (1/2)) |||
                simpleTabbed
 
+-- Floating windows
 myManageHook = composeAll
     [ className =? "Xmessage" --> doFloat
     , title =? "vim-float" --> doFloat
+    , title =? "mpv-float" --> (doF copyToAll) <+> doFloat
     , manageDocks
     ]
 
@@ -115,6 +140,7 @@ main = do
             { terminal = myTerminal
             , workspaces = withScreens nbScreens myVirtualWSs
             , modMask = mod4Mask
+            , focusFollowsMouse = False
             , manageHook = myManageHook <+> manageHook def
             , layoutHook = avoidStruts $ mySpacedLayout $ myBaseLayout
             , logHook = dynamicLogWithPP . marshallPP 0 $ xmobarPP
