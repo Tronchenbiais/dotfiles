ff0c55639516bbf78644c85e0a002506d4969557
Quentin PLAZAR <quentinplaz@hotmail.com>
2020-02-09 10:35:14 +0100

    move config files to 'config/' directory, interactive linking

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -4,26 +4,33 @@ function make_link {
     src=$2 
     if [ -e $src ]
     then
-        echo "$src exists, skipping..."
-    else
-        mkdir -p `dirname $src` 
-        ln -s `pwd`/$target $src
-        echo "$src linked"
+        echo -n "Replace $src? ('n' to keep, anything else overrides)"
+        read replace
+        if [ $replace == "n" ]
+        then
+            echo "$src skipped"
+            echo ""
+            return 0
+        fi
     fi
+    mkdir -p `dirname $src` 
+    ln -sf `pwd`/$target $src
+    echo "$src linked"
+    echo ""
 }
 
-make_link bashrc ~/.bashrc
-make_link inputrc ~/.inputrc
+make_link config/bashrc ~/.bashrc
+make_link config/inputrc ~/.inputrc
 
-make_link gitconfig ~/.gitconfig
+make_link config/gitconfig ~/.gitconfig
 
-make_link lxterminal.conf ~/.config/lxterminal/lxterminal.conf
-make_link vimrc ~/.config/nvim/init.vim
-make_link vifmrc ~/.config/vifm/vifmrc
+make_link config/lxterminal.conf ~/.config/lxterminal/lxterminal.conf
+make_link vim/vimrc ~/.config/nvim/init.vim
+make_link config/vifmrc ~/.config/vifm/vifmrc
 
-make_link xmonad-session-rc ~/.xmonad/xmonad-session-rc
-make_link xmonad.hs ~/.xmonad/xmonad.hs
-make_link compton.conf ~/.config/compton.conf
-make_link rofi.rasi ~/.config/rofi/config.rasi
+make_link config/xmonad-session-rc ~/.xmonad/xmonad-session-rc
+make_link config/xmonad.hs ~/.xmonad/xmonad.hs
+make_link config/compton.conf ~/.config/compton.conf
+make_link config/rofi.rasi ~/.config/rofi/config.rasi
 
 make_link img/middleEarth.jpg ~/wallpaper/wallpaper
--- vimrc
+++
@@ -1,5 +0,0 @@
-let s:path = fnamemodify(resolve(expand('<sfile>:p')), ':h')
-
-let &packpath = s:path . '/vim,' . &packpath
-
-exe 'source ' . s:path . '/vim/vimrc'
