d9821921cde80036264cfbb374918f4ed346d4d1
Quentin PLAZAR <quentinplaz@hotmail.com>
2020-02-09 10:59:17 +0100

    install script

---
+++ install_software.sh
@@ -0,0 +1,12 @@
+# Core
+apt install -y git xorg lightdm compton xmonad xmobar lxterminal xautolock rofi 
+
+# Minimal
+apt install -y neovim fzf vifm feh qutebrowser zathura
+
+# Full
+apt install -y pulseaudio pavucontrol cmus mpv
+
+# Dev
+apt install -y cmake build-essentials ccls
+
