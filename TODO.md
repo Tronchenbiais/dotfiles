0f928024e1f161aa228dbe21c5f2936f1f5667fa
Quentin PLAZAR <quentinplaz@hotmail.com>
2020-02-12 21:59:56 +0100

    xmonad enhancements

--- TODO.md
+++ TODO.md
@@ -1,6 +1,16 @@
-_Less Urgent_
+_Urgent_:
 
+_General_:
 - add a `--force` option to `link_dotfiles.sh`
 - add an option to choose Core, minimal or full package install
 - backup the following configs :
     - qutebrowser
+- find a mail client probably.
+
+_Individual config files_:
+- XMonad
+    - Clickable workspaces in xmobar with `XMonad.*.ServerMode` and xmobar
+      actions
+    - Custom LogHook to synthesise the state of all screens (urgent, which one
+      is active, etc.)
+    - InsertPositions. Do we give focus to new windows ?
--- config/xmonad.hs
+++ config/xmonad.hs
@@ -2,6 +2,7 @@ import Data.List
 import Data.Map (fromList)
 import System.IO
 import XMonad
+import XMonad.Prompt
 import XMonad.Actions.CopyWindow
 import XMonad.Actions.CycleWS
 import XMonad.Actions.OnScreen
@@ -11,9 +12,12 @@ import XMonad.Config.Azerty
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.IndependentScreens
+import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Layout.Tabbed
+import XMonad.Layout.WorkspaceDir
 import XMonad.Util.EZConfig
 import XMonad.Util.Run(spawnPipe) 
 import XMonad.Util.Themes
@@ -80,9 +84,11 @@ myKeys conf =
     [ ("M-b", spawn myBrowser)
     , ("M-<Backspace>", kill)
     , ("M-BS", kill)
+    , ("M-!", spawn (myTerminal ++ " -t lxterminal-float"))
     , ("M-f", spawn (myTerminal ++ " -e vifm"))
     , ("M-S-l", spawn mySreenLockCmd)
     , ("M-p", spawn "rofi -show drun")
+    , ("M-d", changeDir myPromptTheme)
     , ("M-n", onNextNeighbour def W.view)
     , ("M-S-n", composeAll
         [ onNextNeighbour def W.shift
@@ -129,36 +135,48 @@ myTheme = (theme xmonadTheme)
     , fontName = "xft:Courrier:pixelsize=12"
     }
 
+myPromptTheme = def { font = "xft:Courier:pixelSize=13"
+                    , height = 30}
+
+
 tallLayout = Tall 1 (2/100) (1/2)
 tabbedLayout = tabbedBottom shrinkText myTheme
 
-myBaseLayout = tallLayout |||
-               Mirror tallLayout |||
-               tabbedLayout
+myBaseLayout = smartBorders ( tallLayout |||
+                              Mirror tallLayout |||
+                              tabbedLayout )
+
+myCustomLayout = workspaceDir "~" myBaseLayout
 
 -- Floating windows
 myManageHook = composeAll
     [ className =? "Xmessage" --> doFloat
     , title =? "vim-float" --> doFloat
-    , title =? "mpv-float" --> (doF copyToAll) <+> doFloat
+    , title =? "lxterminal-float" --> doFloat
+    , title =? "mpv" --> (doF copyToAll) <+> doFloat
     , manageDocks
     ]
 
 main = do
     status <- spawnPipe "/usr/bin/xmobar"
     nbScreens <- countScreens
-    let baseConfig = ewmh $ docks azertyConfig
+    let baseConfig = ewmh $ withUrgencyHook NoUrgencyHook $ docks azertyConfig
             { terminal = myTerminal
             , workspaces = withScreens nbScreens myVirtualWSs
             , modMask = mod4Mask
+            , normalBorderColor = "black"
+            , focusedBorderColor = "orange"
             , focusFollowsMouse = False
             , manageHook = myManageHook <+> manageHook def
-            , layoutHook = avoidStruts $ mySpacedLayout $ myBaseLayout
+            , layoutHook = avoidStruts $ mySpacedLayout $ myCustomLayout
             , logHook = dynamicLogWithPP . marshallPP 0 $ xmobarPP
                 { ppOutput = hPutStrLn status
                 , ppSep = " - "
                 , ppTitle = xmobarColor "orange" "" . shorten 50
+                , ppCurrent = \s -> xmobarColor "orange" "" $ "[" ++ s ++ "]"
+                , ppVisible = \s -> xmobarColor "green" "" $ "[" ++ s ++ "]"
                 , ppHidden = xmobarColor "white" ""
+                , ppUrgent = xmobarColor "yellow" ""
                 , ppHiddenNoWindows = id
                 }
             } in xmonad $ additionalKeysP baseConfig $ myKeys baseConfig
